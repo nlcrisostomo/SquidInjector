93b7da2bb3987b3e47d9cd737e8defb03986f0ab
nlcrisostomo <nlcrisostomo@gmail.com>
2025-04-08 17:56:49 +1000

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,4 +1,14 @@
 @echo off
+setlocal
+
 set DIR=%~dp0
-set JAVA_HOME=/opt/android-sdk-linux/jdk
-"%DIR%\gradle\wrapper\gradle-wrapper.jar" %*
+set CLASSPATH=%DIR%gradle\wrapper\gradle-wrapper.jar
+
+if exist "%JAVA_HOME%\bin\java.exe" (
+    set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+) else (
+    set JAVA_EXE=java
+)
+
+"%JAVA_EXE%" -cp "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+endlocal
